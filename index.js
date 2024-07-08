854a777dbe3745672c1bc21041548751a79a4b10
zhangfz <2633915958@qq.com>
2024-07-08 14:28:41 +0800

    增加了对移动端的支持

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // @name:zh-TW         更好的 Youtube Shorts
 // @namespace          Violentmonkey Scripts
 // @version            2.0.8
-// @update             2024-07-06
+// @update             2024-07-08
 // @description        Provide more control functions for YouTube Shorts, including automatic/manual redirection to corresponding video pages, volume control, progress bar, auto scrolling, shortcut keys, and more.
 // @description:zh-CN  为 Youtube Shorts提供更多的控制功能，包括自动/手动跳转到对应视频页面，音量控制，进度条，自动滚动，快捷键等等。
 // @description:zh-TW  為 Youtube Shorts提供更多的控制功能，包括自動/手動跳轉到對應影片頁面，音量控制，進度條，自動滾動，快捷鍵等等。
